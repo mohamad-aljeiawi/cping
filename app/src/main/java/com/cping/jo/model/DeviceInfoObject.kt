ec675a04d4246c14c0fe578cdb9a4c3214b42432
mohamad <mobile.mohamad0789@gmail.com>
2025-06-02 01:44:40 +0300

    Add authentication and navigation graphs
    - Introduced `DeviceInfoObject` data class for device information serialization.
    - Implemented `AuthGraph` for handling authentication navigation, including a login screen.
    - Created `MainGraph` for main application navigation, with placeholders for future screens.
    - Added `RootGraph` to manage the overall navigation flow based on user login state.
    - Developed `SplashGraph` for initial splash screen handling.
    - Implemented `LoginScreen` and `LoginViewModel` for user authentication via OTP.
    - Removed unused splash logo image file.

---
+++ app/src/main/java/com/cping/jo/model/DeviceInfoObject.kt
@@ -0,0 +1,3 @@
+package com.cping.jo.model
+
+data class DeviceInfoObject()
---
+++ app/src/main/java/com/cping/jo/navigation/graphs/AuthGraph.kt
@@ -0,0 +1,35 @@
+package com.cping.jo.navigation.graphs
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.cping.jo.screens.auth.login.LoginScreen
+import com.cping.jo.screens.auth.login.LoginViewModel
+
+
+@Composable
+fun AuthGraph(
+    rootNavController: NavHostController,
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = AuthScreens.Login.route
+    ) {
+        composable(AuthScreens.Login.route) {
+            val viewModel = hiltViewModel<LoginViewModel>()
+            LoginScreen(
+                rootNavController = rootNavController,
+                navController = navController,
+                viewModel = viewModel
+            )
+        }
+    }
+}
+
+sealed class AuthScreens(val route: String) {
+    data object Login : AuthScreens(route = "LOGIN")
+}
